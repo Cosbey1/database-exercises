2a45794a0da591142bf5d06c94ae5188096c5bad
CleoBreaux <corey.osbey@gmail.com>
2022-10-25 15:20:29 -0500

    exercises

---
+++ src/main/java/com/example/tablesexercise/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.tablesexercise;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
